dcfa8be6ece3eaa79cc4d7a7a7f33561f6194dac
Flavio <flavioONildo@outlook.com>
2019-06-11 22:18:15 -0400

    Projeto Visual Pagina Criar

---
+++ FibraOticaBlog/FibraOticaBlog/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FibraOticaBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FibraOticaBlog.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Criar(Cliente cliente)
+        {
+            return RedirectToAction("home.cshtml");
+        }
+    }
+}
